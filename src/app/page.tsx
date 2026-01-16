e2b0f30ae58e09526de5e34a3c0ce277d4ee4ee6
Julian Delgado <juliandelgado@alu.frt.edu.ar>
2026-01-16 16:05:39 +1000

    feat: remove TechStack section from homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,6 @@ export default function Home() {
                 <Hero/>
                 <Work/>
                 <Services/>
-                <TechStack/>
                 <Process/>
                 <Pricing/>
                 <FAQ/>
